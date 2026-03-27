4b0d3b31fe12e424fe00a3d07844f2ecc7af30c4
Linukumar Kuttappan <linukumar.kv1983@gmail.com>
2026-03-27 15:57:28 -0700

    Commit lms38

---
+++ lms-day38.js
@@ -0,0 +1,12 @@
+//Write a program that checks if a number is positive, negative, or zero.
+//2. Create a switch statement to display the grade description based on a letter grade (A, B, C, D, F).
+
+let number =30
+
+if (number > 0){
+    console.log(" The number is positive")    
+} else if (number < 0){
+    console.log("The number is negative")    
+} else{
+    console.log("The number is zero")
+}
