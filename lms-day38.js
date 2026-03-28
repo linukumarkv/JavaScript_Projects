3057393868e06762a1b8c5e95e6a345f64420308
linukumarkv <linukumar.kv1983@gmail.com>
2026-03-28 08:28:25 -0700

    Commit lms38 2

--- lms-day38.js
+++ lms-day38.js
@@ -10,3 +10,29 @@ if (number > 0){
 } else{
     console.log("The number is zero")
 }
+
+
+//Create a switch statement to display the grade description based on a letter grade (A, B, C, D, F).
+
+let grade = 'C'
+
+switch (grade){
+    case 'A':
+        console.log("Excellent - Outstanding perfromance!");
+        break;
+    case 'B':
+        console.log("Good - performance!");
+        break;
+    case 'C':
+        console.log("Met the Expectations!");
+        break;
+    case 'D':
+        console.log("eeds Improvement!");
+        break;
+    case 'F':
+        console.log("Did not meet the minimum requirements");
+        break;
+    default:
+    console.log("Invalid Grade - Please enter A, B, C, D, or F");
+}
+   
