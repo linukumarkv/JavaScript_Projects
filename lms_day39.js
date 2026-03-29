95a4ff38ddbeee8c85e068a864fb1be574dc8b96
linukumarkv <linukumar.kv1983@gmail.com>
2026-03-29 16:06:09 -0700

    Commit lms39 Q2

--- lms_day39.js
+++ lms_day39.js
@@ -10,9 +10,23 @@ for (let i =0; i < fruits.length; i++){
     }
 }
 
-//Using forEach
+//Write a program to print only fruits with more than 5 letters using Using forEach
 fruits.forEach(a=>{
     if (a.length> 5){
         console.log(a)
     }
 })
+
+//2. Write a program to find the name of the youngest employee in an array of objects.
+
+let employee = [{'name': 'Linu', age: 40},{'name': 'Kumar', age: 39},{'name': 'Paru', age: 10} ];
+let youngest = employee[0];
+
+employee.forEach(b=>{
+    if (b.age < youngest.age){
+        youngest= b;
+    }
+
+})
+
+console.log(`The Youngest employee is ${youngest.name} at ${youngest.age} years of age`);
